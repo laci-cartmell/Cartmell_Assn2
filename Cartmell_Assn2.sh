b44b4f76ee34b7b704a95c9c812e057f5d19b685
laci <ljc454@utulsa.edu>
2020-09-18 10:00:46 -0500

    Initial Script

---
+++ Cartmell_Assn2.sh
@@ -0,0 +1,11 @@
+#!
+#
+# Extract ID, Year, Trappability from BTS_data.txt for 13BTS010 -- 13BTS089
+#	write to single output file(.gz) that is reverse sorted by Trappability, includes three columns
+#
+cd ../unix_2_files
+	# need in this directory to access files
+
+# create a script that makes another script, write a script that creates a subset, child script executes from parent
+
+
